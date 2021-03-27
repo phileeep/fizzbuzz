b8d6bd2f1065b1e424bc208ce63f4f0656dbdb04
Kane <Kgin888@gmail.com>
2021-03-27 11:19:50 +0000

    added tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,14 @@ describe "fizzbuzz" do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+  it 'returns "buzz" when passed 5' do 
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+  it 'return "fizzbuzz" when passed 15' do 
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+  it 'return "7" when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
 end
+
